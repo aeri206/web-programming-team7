217a9e690e9b06c3b8b3bec4f6592871b180e3b6
jieun <wldms5764@gmail.com>
2020-05-13 16:29:54 +0900

    redirect home after login

--- jurinee/app/controllers/application_controller.rb
+++ jurinee/app/controllers/application_controller.rb
@@ -1,9 +1,27 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    # before_action :configure_permitted_parameters, if: :devise_controller?
  
-    protected
+    # protected
  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:first_name, :last_name, :email, :password, :password_confirmation])
+    # def configure_permitted_parameters
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:first_name, :last_name, :email, :password, :password_confirmation])
+    # end
+
+    before_action :store_user_location!, if: :storable_location?
+    # before_action :authenticate_user! # 로그인한 유저만 볼 수 있음. 랜딩 페이지가 자동으로 로그인으로 들어감 
+
+    private
+    
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
     end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
+    end
+
+    def after_sign_in_path_for(resource_or_scope)
+      stored_location_for(resource_or_scope) || super
+    end
+
 end
