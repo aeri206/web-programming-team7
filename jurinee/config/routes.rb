3ec1db190a819d9021a322ddbb7c8c0fd3eebfda
jieun <wldms5764@gmail.com>
2020-05-13 15:49:59 +0900

    move html&css&image files

--- jurinee/config/routes.rb
+++ jurinee/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get '/home', to: 'home#index'
+
   get '/profile/:id', to: 'profile#index' 
   get '/profile/:id/edit', to: 'profile#edit' 
   post '/profile/:id', to: 'profile#update'
