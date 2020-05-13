85e90dcf3b374bf980bc1efb0754e353e2c7b1cf
jieun <wldms5764@gmail.com>
2020-05-13 16:29:54 +0900

    redirect home after login

--- jurinee/app/controllers/home_controller.rb
+++ jurinee/app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+    
     def index
+        # if !user_signed_in?
+        #     redirect_to 'users/sign_in'
+        # end
         render 'landing_page'
     end
 end
