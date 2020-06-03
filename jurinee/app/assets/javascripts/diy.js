69b3a87a87619208d53f207b0a3031b79cedbbc3
jieun <wldms5764@gmail.com>
2020-06-03 16:45:16 +0900

    filtering design & popovers

--- jurinee/app/assets/javascripts/diy.js
+++ jurinee/app/assets/javascripts/diy.js
@@ -1,1 +1,13 @@
-//= require material
+// = require jquery3
+// = require popper
+// = require bootstrap-sprockets
+
+$(function () {
+    $('[data-toggle="popover"]').popover({
+        html: true
+    });
+})
+
+// $(document).ready(() => {
+//     alert("be careful!");
+// })
